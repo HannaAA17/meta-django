afa345d471432fcfdd730ea41427e337470b7e01
Alberto Hanna <alberto.adell17@gmail.com>
2023-02-01 23:20:13 +0200

    C6W2 LittleLemon menu

---
+++ C6 apis/labs/LittleLemon/LittleLemonDRF/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from .models import Menuitem
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menuitem
+        fields = ('id', 'title', 'price', 'inventory')
+        extra_kwargs = {
+            'price': {'min_value':2},
+            'inventory': {'min_value':0},
+        }
--- C6 apis/labs/LittleLemon/LittleLemonDRF/views.py
+++ C6 apis/labs/LittleLemon/LittleLemonDRF/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
 
+from .models import Menuitem
+from .serializers import MenuItemSerializer
 # Create your views here.
+
+class MenuItemListView(generics.ListCreateAPIView):
+    queryset = Menuitem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class singleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menuitem.objects.all()
+    serializer_class = MenuItemSerializer
