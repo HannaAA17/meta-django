c604c780815c0bb4f150f4e1c2ee67c9637f329a
Alberto Hanna <alberto.adell17@gmail.com>
2023-07-19 08:14:28 +0000

    start proj

---
+++ install.sh
@@ -0,0 +1,10 @@
+# sudo apt-get update -y && sudo apt-get upgrade -y
+# sudo apt-get install mysql-server mysql-client -y
+# sudo service mysql start
+
+# sudo mysql -u root -e "CREATE DATABASE reservations;"
+# sudo mysql -u root -e "CREATE USER 'admindjango'@'localhost' IDENTIFIED BY 'employee@123!';"
+# sudo mysql -u root -e "GRANT ALL ON *.* TO 'admindjango'@'localhost';"
+
+
+
